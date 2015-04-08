cdf1c39d5e8360bdf3247969e5f4c103a4035a6c
Marco de Jongh <m.jongh88@gmail.com>
2015-04-08 12:44:10 +0200

    Added travis

--- setup.sh
+++ setup.sh
@@ -8,3 +8,5 @@ ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/
 
 # Install brew packages
 brew install android-sdk autoconf casperjs coreutils findutils gawk gdbm gettext git git-extras glib gmp gnu-indent gnu-sed gnu-tar gnutls grep hub libevent libffi libpng libssh2 libtasn1 maven midnight-commander mongodb ncdu nettle nmap node openssh openssl pcre phantomjs pkg-config reattach-to-user-namespace s-lang ssh-copy-id tmux wget xz zsh
+
+gem install travis
