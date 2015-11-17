8ec60f772046b60a9e76286774f258551e7371c5
Marco de Jongh <m.jongh88@gmail.com>
2015-11-17 10:31:52 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -7,7 +7,7 @@ defaults write com.apple.dock static-only -bool TRUE
 ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
 # Install brew packages
-brew install android-sdk autoconf casperjs coreutils findutils gawk gdbm gettext git git-extras glib gmp gnu-indent gnu-sed gnu-tar gnutls grep hub libevent libffi libpng libssh2 libtasn1 maven midnight-commander mongodb ncdu nettle nmap node openssh openssl pcre phantomjs pkg-config reattach-to-user-namespace s-lang ssh-copy-id tmux wget xz zsh
+brew install autoconf casperjs coreutils findutils gawk gdbm gettext git git-extras glib gmp gnu-indent gnu-sed gnu-tar gnutls grep hub libevent libffi libpng libssh2 libtasn1 maven midnight-commander mongodb ncdu nettle nmap node openssh openssl pcre phantomjs pkg-config reattach-to-user-namespace s-lang ssh-copy-id tmux wget xz zsh
 
 gem install travis
 brew cask install macdown
