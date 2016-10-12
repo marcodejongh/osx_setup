ee4c9f49eed250a348ecded5b22e7cc4eac8246b
Marco de Jongh <m.jongh88@gmail.com>
2016-10-13 10:13:33 +1100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -17,9 +17,39 @@ unzip BeardedSpice-latest.zip -d /Applications
 
 mkdir ~/Screenshots
 
-# Change default dir for screenshots
+
+echo "Change default dir for screenshots to ~/Screenshots"
 defaults write com.apple.screencapture location ~/Screenshots 
 
-# Change screenshot format to jpg
+echo "Change screenshot format to jpg to save space"
 defaults write com.apple.screencapture type jpg
+
+# source: https://gist.github.com/brandonb927/3195465
+echo "Disable Photos.app from starting everytime a device is plugged in"
+defaults -currentHost write com.apple.ImageCapture disableHotPlug -bool true
+
+echo "Increasing sound quality for Bluetooth headphones/headsets"
+defaults write com.apple.BluetoothAudioAgent "Apple Bitpool Min (editable)" -int 40
+
+echo "Show hidden files in Finder by default"
+defaults write com.apple.Finder AppleShowAllFiles -bool true
+
+echo "Show dotfiles in Finder by default"
+defaults write com.apple.finder AppleShowAllFiles TRUE
+
+echo "Show all filename extensions in Finder by default"
+defaults write NSGlobalDomain AppleShowAllExtensions -bool true
+
+echo "Display full POSIX path as Finder window title"
+defaults write com.apple.finder _FXShowPosixPathInTitle -bool true
+
+echo "Disable the warning when changing a file extension"
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool false
+
+echo "Avoid creation of .DS_Store files on network volumes"
+defaults write com.apple.desktopservices DSDontWriteNetworkStores -bool true
+
+echo "Prevent Time Machine from prompting to use new hard drives as backup volume"
+defaults write com.apple.TimeMachine DoNotOfferNewDisksForBackup -bool true
+
 killall SystemUIServer
